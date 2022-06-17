bd5e478ebecc148c01994d338de095cb700806c1
Johnson Cho <johnsoncho@Roys-MBP.gateway>
2022-06-18 08:26:44 +1000

    moved global variables into object

--- scripts.js
+++ scripts.js
@@ -1,54 +1,91 @@
 const boardContainer = document.querySelector(".boardcontainer");
-const clickMemory = [];
-const playerOneMemory = [];
-const playerTwoMemory = [];
+const gameState = {
+  playerOneMemory: [],
+  playerTwoMemory: [],
+  winConditions: [
+    //rows
+    [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ],
+    //columns
+    [
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+    ],
+    //Diagonals
+    [
+      [1, 5, 9],
+      [3, 5, 7],
+    ],
+  ],
+};
+// const playerOneMemory = [];
+// const playerTwoMemory = [];
 
-const winConditions = [
-  //rows
-  [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ],
-  //columns
-  [
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-  ],
-  //Diagonals
-  [
-    [1, 5, 9],
-    [3, 5, 7],
-  ],
-];
+// const winConditions = [
+//   //rows
+//   [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//   ],
+//   //columns
+//   [
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [3, 6, 9],
+//   ],
+//   //Diagonals
+//   [
+//     [1, 5, 9],
+//     [3, 5, 7],
+//   ],
+// ];
 
+//Use to check board class assignment working
 const boardStateCheck = () => {
   const boardState = document.querySelectorAll(".grid");
   return console.log(boardState);
 };
 
+//Track and check whos turn it is
 const TurnCheck = (param) => {
-  if (playerOneMemory.length === 0) {
+  if (gameState.playerOneMemory.length === 0) {
     playerOneClick(param);
-  } else if (playerTwoMemory.length < playerOneMemory.length) {
+  } else if (
+    gameState.playerTwoMemory.length < gameState.playerOneMemory.length
+  ) {
     playerTwoClick(param);
-  } else if (playerOneMemory.length < playerTwoMemory.length) {
+  } else if (
+    gameState.playerOneMemory.length < gameState.playerTwoMemory.length
+  ) {
     playerOneClick(param);
   } else {
     playerOneClick(param);
   }
 };
 
+//Check if square has one of the classes added and refuse
+const occupiedSquareCheck = (event) => {
+  if (
+    !event.target.classList.contains("playerOneClick") ||
+    !event.target.classList.contains("playerTwoClick")
+  ) {
+    event.target.classList.add("playerOneClick");
+  }
+};
+
 //Jank Win Checker
 const stringChecker = () => {
-  let sString = "";
-  for (const criterias of winConditions) {
+  for (const criterias of gameState.winConditions) {
     for (const condition of criterias) {
       let sCondition = condition.join("");
-      let pOneString = playerOneMemory.sort().join("");
+      let pOneString = gameState.playerOneMemory.sort().join("");
       console.log(pOneString);
-      if (pOneString.indexOf(sCondition) !== -1) {
+      if (pOneString.includes(sCondition)) {
         alert("player wins");
       }
     }
@@ -59,11 +96,10 @@ const playerOneClick = (param) => {
   const clickedChildEle = param.target;
   if (param.currentTarget !== clickedChildEle) {
     param.target.classList.add("playerOneClick");
-    playerOneMemory.push(param.target.id);
-    checkWin(playerOneMemory);
+    gameState.playerOneMemory.push(param.target.id);
     stringChecker();
-    console.log(playerOneMemory);
-    console.log(playerTwoMemory);
+    console.log(gameState.playerOneMemory);
+    console.log(gameState.playerTwoMemory);
     console.log("player1running");
   }
 };
@@ -72,17 +108,11 @@ const playerTwoClick = (param) => {
   const clickedChildEle = param.target;
   if (param.currentTarget !== clickedChildEle) {
     param.target.classList.add("playerTwoClick");
-    playerTwoMemory.push(param.target.id);
-    checkWin(playerTwoMemory);
-    console.log(playerTwoMemory);
+    gameState.playerTwoMemory.push(param.target.id);
+    stringChecker();
+    console.log(gameState.playerTwoMemory);
     console.log("player2running");
   }
-};
-
-const clickWriteToStorage = (param) => {
-  const clickedChildEle = param.target;
-  clickMemory.push(param.target.outerHTML);
-  console.log(clickMemory);
 };
 
 // boardContainer.addEventListener("click", clickWriteToStorage);
