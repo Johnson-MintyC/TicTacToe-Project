d9339a7e36ac5f0f4ee6b6c2a9d6d014a94ecd7a
Johnson Cho <johnsoncho@Roys-MBP.gateway>
2022-06-19 20:11:07 +1000

    put draw check after win condition

--- scripts.js
+++ scripts.js
@@ -5,22 +5,16 @@ const arrOfSquares = document.querySelectorAll(".grid");
 const gameState = {
   winConditions: [
     //rows
-    [
-      ["1", "2", "3"],
-      ["4", "5", "6"],
-      ["7", "8", "9"],
-    ],
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
     //columns
-    [
-      ["1", "4", "7"],
-      ["2", "5", "8"],
-      ["3", "6", "9"],
-    ],
+    ["1", "4", "7"],
+    ["2", "5", "8"],
+    ["3", "6", "9"],
     //Diagonals
-    [
-      ["1", "5", "9"],
-      ["3", "5", "7"],
-    ],
+    ["1", "5", "9"],
+    ["3", "5", "7"],
   ],
   roundOver: false,
   resetButton: document.querySelector(".resetButton"),
@@ -32,7 +26,9 @@ const playerOne = {
   score: 0,
   memory: [],
   HTMLScoreBoard: document.querySelector("#p1Scoreboard"),
-  HTMLName: document.querySelector("p1Handle"),
+  HTMLName: document.querySelector("#p1Handle"),
+  HTMLChangeNameButton: document.querySelector("#p1NameChange"),
+  HTMLpic: document.querySelector("#p1pic"),
 };
 
 const playerTwo = {
@@ -41,7 +37,8 @@ const playerTwo = {
   score: 0,
   memory: [],
   HTMLScoreBoard: document.querySelector("#p2Scoreboard"),
-  HTMLName: document.querySelector("p2Handle"),
+  HTMLName: document.querySelector("#p2Handle"),
+  HTMLChangeNameButton: document.querySelector("#p2NameChange"),
 };
 
 //Use to check board state class assignment working, Debugging purposes
@@ -65,7 +62,7 @@ const TurnCheck = (param) => {
   }
 };
 
-//Check if square has one of the classes added and refuse
+// Check if square has one of the classes added and refuse
 const occupiedSquareCheck = (event, player, whoMemory) => {
   if (
     !event.target.classList.contains("playerOne") &&
@@ -94,20 +91,21 @@ const increaseScore = (player) => {
 
 //Check win array is subset of the player superset
 const winChecker = (player) => {
-  if (playerOne.memory.length === 5 && playerTwo.memory.length === 4) {
-    alert("It's a Draw");
-  } else {
-    for (const criterias of gameState.winConditions) {
-      for (const condition of criterias) {
-        if (condition.every((element) => player.memory.includes(element))) {
-          alert(player.name + "win");
-          increaseScore(player);
-          gameState.roundOver = true;
-          console.log(gameState.roundOver);
-          // test.TextContent = "Score: " + player.score;
-        }
-      }
+  for (const criterias of gameState.winConditions) {
+    if (criterias.every((element) => player.memory.includes(element))) {
+      alert(player.name + " Wins!");
+      increaseScore(player);
+      gameState.roundOver = true;
+      console.log(gameState.roundOver);
+      // test.TextContent = "Score: " + player.score;
     }
+  }
+  if (
+    playerOne.memory.length === 5 &&
+    playerTwo.memory.length === 4 &&
+    gameState.roundOver === false
+  ) {
+    alert("Its a Draw");
   }
 };
 
@@ -141,11 +139,31 @@ const gameReset = () => {
   }
   playerOne.memory = [];
   playerTwo.memory = [];
+  gameState.roundOver = false;
   console.log("resert click");
   console.log(playerOne.score, playerTwo.score);
 };
 
-//change name of Player 1 Profile
-const changeName = (player) => {};
+//change name of Player 1 HTML Profile
+const changePlayerOneName = () => {
+  const playerInput = prompt("Enter name: ");
+  playerOne.name = playerInput;
+  playerOne.HTMLName.innerText = playerInput;
+};
+
+//change name of Player 2 HTML Profile
+const changePlayerTwoName = () => {
+  const playerInput = prompt("Enter name: ");
+  playerTwo.name = playerInput;
+  playerTwo.HTMLName.innerText = playerInput;
+};
 
 gameState.resetButton.addEventListener("click", gameReset);
+
+//Listener to change Name for P1
+playerOne.HTMLChangeNameButton.addEventListener("click", changePlayerOneName);
+
+//Listener to change Name for P2
+playerTwo.HTMLChangeNameButton.addEventListener("click", changePlayerTwoName);
+
+console.log(playerOne.HTMLpic.src);
