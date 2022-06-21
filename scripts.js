88154ffc60920f3473e9c9a9435e41cf7c30cb91
Johnson Cho <johnsoncho@Roys-MBP.gateway>
2022-06-22 09:17:13 +1000

    found less repetitive way for the hard ai to work

--- scripts.js
+++ scripts.js
@@ -276,53 +276,70 @@ const AiToggle = () => {
 //   }
 // };
 
-//Harder AI first attempt
-//RowChecker, checks if 3rd Square
-const rowchecker = (square1, square2, square3) => {
-  if (
-    square1.classList.contains("playerOne") &&
-    square2.classList.contains("playerOne") &&
-    !square3.classList.contains("playerTwo")
-  ) {
-    return square3;
-  } else if (
-    square2.classList.contains("playerOne") &&
-    square3.classList.contains("playerOne") &&
-    !square1.classList.contains("playerTwo")
-  ) {
-    return square1;
-  } else if (
-    square1.classList.contains("playerOne") &&
-    square3.classList.contains("playerOne") &&
-    !square2.classList.contains("playerTwo")
-  ) {
-    return square2;
-  } else {
-    return false;
+//Harder AI cleaned up
+
+//Row Checker that loops through, using wining combo as relationships to check
+const RowCheckerV2 = () => {
+  for (criterias of gameState.winConditions) {
+    let critID1 = "#" + criterias[0];
+    let critID2 = "#" + criterias[1];
+    let critID3 = "#" + criterias[2];
+    let square1 = document.querySelector(critID1);
+    let square2 = document.querySelector(critID2);
+    let square3 = document.querySelector(critID3);
+    if (
+      square1.classList.contains("playerOne") &&
+      square2.classList.contains("playerOne") &&
+      !square3.classList.contains("playerTwo")
+    ) {
+      console.log(square3);
+      return square3;
+    } else if (
+      square2.classList.contains("playerOne") &&
+      square3.classList.contains("playerOne") &&
+      !square1.classList.contains("playerTwo")
+    ) {
+      console.log(square1);
+      return square1;
+    } else if (
+      square1.classList.contains("playerOne") &&
+      square3.classList.contains("playerOne") &&
+      !square2.classList.contains("playerTwo")
+    ) {
+      console.log(square2);
+      return square2;
+    }
   }
 };
-//Go for win, if third empty
-const WinRowchecker = (square1, square2, square3) => {
-  if (
-    square1.classList.contains("playerTwo") &&
-    square2.classList.contains("playerTwo") &&
-    !square3.classList.contains("playerOne")
-  ) {
-    return square3;
-  } else if (
-    square2.classList.contains("playerTwo") &&
-    square3.classList.contains("playerTwo") &&
-    !square1.classList.contains("playerOne")
-  ) {
-    return square1;
-  } else if (
-    square1.classList.contains("playerTwo") &&
-    square3.classList.contains("playerTwo") &&
-    !square2.classList.contains("playerOne")
-  ) {
-    return square2;
-  } else {
-    return false;
+
+//WinRowCheckerV2
+const WinRowCheckerV2 = () => {
+  for (criterias of gameState.winConditions) {
+    let critID1 = "#" + criterias[0];
+    let critID2 = "#" + criterias[1];
+    let critID3 = "#" + criterias[2];
+    let square1 = document.querySelector(critID1);
+    let square2 = document.querySelector(critID2);
+    let square3 = document.querySelector(critID3);
+    if (
+      square1.classList.contains("playerTwo") &&
+      square2.classList.contains("playerTwo") &&
+      !square3.classList.contains("playerOne")
+    ) {
+      return square3;
+    } else if (
+      square2.classList.contains("playerTwo") &&
+      square3.classList.contains("playerTwo") &&
+      !square1.classList.contains("playerOne")
+    ) {
+      return square1;
+    } else if (
+      square1.classList.contains("playerTwo") &&
+      square3.classList.contains("playerTwo") &&
+      !square2.classList.contains("playerOne")
+    ) {
+      return square2;
+    }
   }
 };
 
@@ -336,97 +353,15 @@ const pushintoAImemory = (square) => {
 const hardAI = () => {
   if (playerTwo.ai === true) {
     if (gameState.roundOver !== true) {
-      const sq1 = document.querySelector("#sq1");
-      const sq2 = document.querySelector("#sq2");
-      const sq3 = document.querySelector("#sq3");
-      const sq4 = document.querySelector("#sq4");
-      const sq5 = document.querySelector("#sq5");
-      const sq6 = document.querySelector("#sq6");
-      const sq7 = document.querySelector("#sq7");
-      const sq8 = document.querySelector("#sq8");
-      const sq9 = document.querySelector("#sq9");
       if (playerOne.memory.length + playerTwo.length != 9) {
         if (playerTwo.memory.length < playerOne.memory.length) {
           let freeSquare = "";
           //Go for wins
-          if (WinRowchecker(sq1, sq5, sq9)) {
-            freeSquare = WinRowchecker(sq1, sq5, sq9);
+          if (WinRowCheckerV2()) {
+            freeSquare = WinRowCheckerV2();
             pushintoAImemory(freeSquare);
-          }
-          //Diagonal right to left
-          else if (WinRowchecker(sq3, sq5, sq7)) {
-            freeSquare = WinRowchecker(sq3, sq5, sq7);
-            pushintoAImemory(freeSquare);
-          }
-          //horizontal top row test
-          else if (WinRowchecker(sq1, sq2, sq3)) {
-            freeSquare = WinRowchecker(sq1, sq2, sq3);
-            pushintoAImemory(freeSquare);
-          }
-          //horizontal middle test
-          else if (WinRowchecker(sq4, sq5, sq6)) {
-            freeSquare = WinRowchecker(sq4, sq5, sq6);
-            pushintoAImemory(freeSquare);
-          }
-          //horizontal bottom test
-          else if (WinRowchecker(sq7, sq8, sq9)) {
-            freeSquare = WinRowchecker(sq7, sq8, sq9);
-            pushintoAImemory(freeSquare);
-          }
-          //vertical left test
-          else if (WinRowchecker(sq1, sq4, sq7)) {
-            freeSquare = WinRowchecker(sq1, sq4, sq7);
-            pushintoAImemory(freeSquare);
-          }
-          //vertical mid test
-          else if (WinRowchecker(sq2, sq5, sq8)) {
-            freeSquare = WinRowchecker(sq2, sq5, sq8);
-            pushintoAImemory(freeSquare);
-          }
-          //vertical right test
-          else if (WinRowchecker(sq3, sq6, sq9)) {
-            freeSquare = WinRowchecker(sq3, sq6, sq9);
-            pushintoAImemory(freeSquare);
-          }
-
-          //Diagonal left to right
-          else if (rowchecker(sq1, sq5, sq9)) {
-            freeSquare = rowchecker(sq1, sq5, sq9);
-            pushintoAImemory(freeSquare);
-          }
-          //Diagonal right to left
-          else if (rowchecker(sq3, sq5, sq7)) {
-            freeSquare = rowchecker(sq3, sq5, sq7);
-            pushintoAImemory(freeSquare);
-          }
-          //horizontal top row test
-          else if (rowchecker(sq1, sq2, sq3)) {
-            freeSquare = rowchecker(sq1, sq2, sq3);
-            pushintoAImemory(freeSquare);
-          }
-          //horizontal middle test
-          else if (rowchecker(sq4, sq5, sq6)) {
-            freeSquare = rowchecker(sq4, sq5, sq6);
-            pushintoAImemory(freeSquare);
-          }
-          //horizontal bottom test
-          else if (rowchecker(sq7, sq8, sq9)) {
-            freeSquare = rowchecker(sq7, sq8, sq9);
-            pushintoAImemory(freeSquare);
-          }
-          //vertical left test
-          else if (rowchecker(sq1, sq4, sq7)) {
-            freeSquare = rowchecker(sq1, sq4, sq7);
-            pushintoAImemory(freeSquare);
-          }
-          //vertical mid test
-          else if (rowchecker(sq2, sq5, sq8)) {
-            freeSquare = rowchecker(sq2, sq5, sq8);
-            pushintoAImemory(freeSquare);
-          }
-          //vertical right test
-          else if (rowchecker(sq3, sq6, sq9)) {
-            freeSquare = rowchecker(sq3, sq6, sq9);
+          } else if (RowCheckerV2()) {
+            freeSquare = RowCheckerV2();
             pushintoAImemory(freeSquare);
           } else if (gameState.unoccupiedSquares.length != 0) {
             freeSquare =
@@ -443,98 +378,10 @@ const hardAI = () => {
         }
       }
     }
-    winChecker(playerTwo);
-    TurnUpdateNotice();
   }
+  winChecker(playerTwo);
+  TurnUpdateNotice();
 };
-
-//Harder AI Second Attempt
-// const nearWin = [
-//   //TopRowcheck
-//   ["sq1", "sq2"],
-//   ["sq2", "sq3"],
-//   ["sq1", "sq3"],
-//   //MidRowCheck
-//   ["sq4", "sq5"],
-//   ["sq5", "sq6"],
-//   ["sq4", "sq6"],
-//   //BottRowCheck
-//   ["sq7", "sq8"],
-//   ["sq8", "sq9"],
-//   ["sq7", "sq9"],
-//   //FirstCol
-//   ["sq1", "sq4"],
-//   ["sq4", "sq7"],
-//   ["sq1", "sq7"],
-//   //SecondCol
-//   ["sq2", "sq5"],
-//   ["sq5", "sq8"],
-//   ["sq2", "sq8"],
-//   //ThirdCol
-//   ["sq3", "sq6"],
-//   ["sq6", "sq9"],
-//   ["sq3", "sq9"],
-//   //DiagLeftToRight
-//   ["sq1", "sq5"],
-//   ["sq5", "sq9"],
-//   ["sq1", "sq9"],
-//   //DiagRightToLeft
-//   ["sq3", "sq5"],
-//   ["sq5", "sq7"],
-//   ["sq3", "sq7"],
-// ];
-
-// const bestMoveForAi = () => {
-//   for (near of nearWin) {
-//     let currentBestMove = "";
-//     if (near.every((element) => playerOne.memory.includes(element))) {
-//       for (criterias of gameState.winConditions) {
-//         if (near.every((element) => criterias.includes(element))) {
-//           let difference = criterias.filter((x) => !near.includes(x));
-//           const convertID = "#" + difference;
-//           const squareToTry = document.querySelector(convertID);
-//           if (!squareToTry.classList.contains("playerTwo")) {
-//             console.log(difference);
-//             currentBestMove = difference;
-//             return difference;
-//           }
-//         }
-//       }
-//     }
-//   }
-// };
-
-// const MediumAi = () => {
-//   if (playerTwo.ai === true) {
-//     if (gameState.roundOver !== true) {
-//     }
-//     if (playerTwo.memory.length < playerOne.memory.length) {
-//       if (playerOne.memory.length + playerTwo.length != 9 && bestMoveForAi()) {
-//         winChecker(playerTwo);
-//         const currentMove = bestMoveForAi();
-//         const convertID = "#" + currentMove;
-//         console.log(currentMove);
-//         const squareToTry = document.querySelector(convertID);
-//         squareToTry.classList.add("playerTwo");
-//         playerTwo.memory.push(currentMove);
-//         TurnUpdateNotice();
-//         winChecker(playerTwo);
-//       } else if (playerOne.memory.length + playerTwo.length != 9) {
-//         winChecker(playerTwo);
-//         freeSquare =
-//           gameState.unoccupiedSquares[
-//             Math.ceil(Math.random() * gameState.unoccupiedSquares.length - 1)
-//           ];
-//         playerTwo.memory.push(freeSquare.id);
-//         const idConvert = "#" + freeSquare.id;
-//         const extract = document.querySelector(idConvert);
-//         extract.classList.add("playerTwo");
-//         TurnUpdateNotice();
-//         winChecker(playerTwo);
-//       }
-//     }
-//   }
-// };
 
 //Return array of squares with no P1 or P2 class, therefore unoccupied
 const unoccupiedListforAi = () => {
